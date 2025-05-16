cc49b122f796c67d4b73126cc8091db2f4b08065
Savas Vedova <savas@stormkit.io>
2025-05-16 17:43:13 +0300

    chore: initial commit

---
+++ main.rs
@@ -0,0 +1,31 @@
+extern crate time;
+
+use std::io::Write;
+use std::net::TcpListener;
+
+fn main() {
+    let port = std::env::var("PORT").unwrap_or_else(|_| "8080".to_string());
+    let address = format!("127.0.0.1:{}", port);
+
+    // Bind allows us to create a connection on the port
+    // and gets it ready to accept connections.
+    let listener = TcpListener::bind(address).unwrap();
+
+    // The listener's accept method waits or 'blocks' until
+    // we have a connection and then returns a new TcpStream
+    // that we can read and write data to.
+    let mut stream = listener.accept().unwrap().0;
+    let message    = "Hello, World!";
+    let format     = "%a, %d %b %Y %T GMT";
+    let time       = time::now_utc();
+    let response   = format!("HTTP/1.1 200 OK\r\n\
+                              Date: {}\r\n\
+                              Content-Type: text/html; charset=utf-8\r\n\
+                              Content-Length: {}\r\n\
+                              \r\n\
+                              {}",
+                              time::strftime(format, &time).unwrap(),
+                              message.len(),
+                              message);
+    let _          = stream.write(response.as_bytes());
+}
